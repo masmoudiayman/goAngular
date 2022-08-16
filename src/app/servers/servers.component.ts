41e4d51a2a2e64db8e6ae16d75b16a1b4fc9fba8
masmoudiayman <aymanmasmoudi7@gmail.com>
2022-08-16 22:50:09 +0100

    16/08/2022

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isEmpty } from 'rxjs';
 
 @Component({
   selector: 'app-servers',
@@ -7,18 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer=false;
-  serverCreationStatus= "No server was created!"
-
+  serverCreated = false;
+  serverCreationStatus = "No server was created!";
+  ServerName = "TestServer";
+  Username="ayman";
+  UsernameCreationStatus ="No Name was added";
+  allowNewUserName=false;
+  servers=['Testserver','Testserver2']
+  ShowDispaly= false;
+  log=[];
   constructor() {
     setTimeout(()=>{
       this.allowNewServer=true;
     },2000);
+
    }
 
   ngOnInit(): void {
   }
   onCreateServer(){
-    this.serverCreationStatus="server was created"
+    this.serverCreated=true;
+    this.servers.push(this.ServerName);
+    this.serverCreationStatus="server was created! with the Name "+ this.ServerName;
   }
+  onUdateServerName(event:Event){
+    this.ServerName = (event.target as HTMLInputElement).value;  
+  }
+  AddUserName(){
+    this.UsernameCreationStatus="Username : "+ this.Username + " was added "
+  }
+  UpdateUsername(event:Event){
+    this.Username = (event.target as HTMLInputElement).value;  
+  } 
+  onToggleDisplay(){
+    this.ShowDispaly =!this.ShowDispaly;
+    this.log.push(this.log.length + 1);
+  }
+
+
 
 }
